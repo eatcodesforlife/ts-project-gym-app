8d219725d87314a5dc83a08f9fe1d3b84f8e039f
Cris Acosta <iamcris87@gmail.com>
2023-01-07 02:20:14 -0800

    started home page

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -15,41 +15,60 @@ import useMediaQuery from '@/hooks/useMediaQuery';
 const Home = ({setSelectedPage}: Props) => {
 
     const isAboveMediumScreen = useMediaQuery('(min-width: 1060px)')
-    console.log(HomePageText)
   return <section id='home' className='gap-16 bg-gray-20 py-10 md:h-full md:pb-10'>
     {/*  image and main header */}
-    <div>
+    <div className='md:flex mx-auto w-5/6 items-center justify-center'>
       {/* main header */}
-      <div>
-        <div>
-          {/* headings */}
+      <div >
+        <div >
           <div>
+            {/* headings */}
             <div>
-              <img src={HomePageText} alt="home-page-text" />
+              <div>
+                <img src={HomePageText} alt="home-page-text" />
+              </div>
             </div>
+            <p className="mt-8 text-sm">
+              Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
+              Studios to get the Body Shapes That you Dream of.. Get Your Dream
+              Body Now.
+            </p>
           </div>
-          <p className="mt-8 text-sm">
-            Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
-            Studios to get the Body Shapes That you Dream of.. Get Your Dream
-            Body Now.
-          </p>
-        </div>
 
-        <ActionButton setSelectedPage={setSelectedPage}>
-          Join now
-        </ActionButton>
-        <AnchorLink 
-          className="text-sm font-bold text-primary-500 
-          underline hover:text-secondary-500 transition duration-300"
-          href={`${NavMenuLinks.ContactUs}`}
-          onClick={() => setSelectedPage(NavMenuLinks.ContactUs)}
-        >
-          Learn more
-        </AnchorLink>
+          <ActionButton setSelectedPage={setSelectedPage}>
+            Join now
+          </ActionButton>
+          <AnchorLink 
+            className="text-sm font-bold text-primary-500 
+            underline hover:text-secondary-500 transition duration-300"
+            href={`${NavMenuLinks.ContactUs}`}
+            onClick={() => setSelectedPage(NavMenuLinks.ContactUs)}
+          >
+            <p>Learn More</p>
+          </AnchorLink>
+        </div>
       </div>
       {/* image */}
+      <div>
+        <img src={HomePageGraphic} alt="home page graphic" />
+      </div>
     </div>
+    {/* sponsors */}
+    {
+      isAboveMediumScreen && (
+        <div>
+          <div>
+            <div >
+              <img src={SponsorForbes} alt="sponsor - forbes" />
+              <img src={SponsorFortune} alt="sponsor - fortune" />
+              <img src={SponsorRedBull} alt="sponsor - Redbull" />
+            </div>
+          </div>
+        </div>
+      )
+    }
   </section>
+
 }
 
 export default Home
